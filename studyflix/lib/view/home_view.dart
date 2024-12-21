f0bc0a01c372dbbc4e5499cc23d611ff99585b3a
grgnitika <gurungnitika59@gmail.com>
2024-12-21 23:14:23 +0545

    home view updated

--- studyflix/lib/view/home_view.dart
+++ studyflix/lib/view/home_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:studyflix/view/home_screen.dart';
 import 'package:studyflix/view/profile_screen.dart';
+import 'package:studyflix/view/saved_screen.dart';
+import 'package:studyflix/view/search_screen.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -13,10 +16,10 @@ class _HomeViewState extends State<HomeView> {
 
   // List of screens for Bottom Navigation
   final List<Widget> _screens = [
-    const Center(child: Text("Home Screen")),
-    const Center(child: Text("Saved Screen")),
-    const Center(child: Text("Search Screen")),
-    const ProfileScreen(), // Updated to navigate to ProfileScreen
+    HomeScreen(),
+    const DownloadsScreen(),
+    const SearchScreen(),
+    const ProfileScreen(),
   ];
 
   void _onItemTapped(int index) {
@@ -28,27 +31,12 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Container(), // Removes the default back arrow
-        title: const Text(
-          "StudyFlix",
-          style: TextStyle(
-            color: Colors.white, // Set the title font color to white
-          ),
-        ),
-        backgroundColor: Colors.blue,
-        elevation: 0, // Optional: to remove shadow for a cleaner look
-        toolbarHeight:
-            kToolbarHeight, // Optional: ensures the height of the AppBar remains consistent
-        automaticallyImplyLeading:
-            false, // To prevent default leading widget like the back button
-      ),
       body: _screens[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.blue, // Set the background color to blue
+        backgroundColor: Colors.black, // Set the background color to blue
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
-        selectedItemColor: Colors.white, // Set the selected icon color to white
+        selectedItemColor: Colors.blue, // Set the selected icon color to white
         unselectedItemColor:
             Colors.white, // Set the unselected icon color to white
         type: BottomNavigationBarType
@@ -60,7 +48,7 @@ class _HomeViewState extends State<HomeView> {
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.bookmark),
-            label: 'Saved',
+            label: 'My Playlist',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.search),
