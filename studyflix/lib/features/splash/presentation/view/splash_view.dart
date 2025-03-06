8851aef1eae41acf4854daad75f04ed3a967ebd5
grgnitika <gurungnitika59@gmail.com>
2025-03-07 00:04:28 +0545

    splash updates

--- studyflix/lib/features/splash/presentation/view/splash_view.dart
+++ studyflix/lib/features/splash/presentation/view/splash_view.dart
@@ -1,94 +1,58 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:studyflix/features/splash/presentation/view_model/splash_cubit.dart';
 
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  State<SplashView> createState() => _SplashViewState();
 }
 
-class _SplashScreenState extends State<SplashScreen>
-    with TickerProviderStateMixin {
-  late final AnimationController _logoController;
-  late final Animation<double> _logoScaleAnimation;
-  late final Animation<double> _logoJumpAnimation;
-
+class _SplashViewState extends State<SplashView> {
   @override
   void initState() {
     super.initState();
-
-    // Initialize the animation controller
-    _logoController = AnimationController(
-      vsync: this,
-      duration: const Duration(
-          seconds: 2), // Increased duration for better visibility
-    );
-
-    // Scale animation for "pop" effect
-    _logoScaleAnimation = CurvedAnimation(
-      parent: _logoController,
-      curve: Curves.easeOutBack,
-    );
-
-    // Jump animation for "bounce" effect
-    _logoJumpAnimation = Tween<double>(begin: 0, end: -30).animate(
-      CurvedAnimation(
-        parent: _logoController,
-        curve: Curves.bounceOut,
-      ),
-    );
-
-    // Start the animation
-    _logoController.forward();
-
-    // Navigate to the next screen after the splash screen
-    Timer(const Duration(seconds: 3), () {
-      Navigator.pushReplacementNamed(context, '/onboarding');
-    });
-  }
-
-  @override
-  void dispose() {
-    _logoController.dispose();
-    super.dispose();
+    context.read<SplashCubit>().init(context);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        color: Colors.black,
-        child: Center(
-          child: AnimatedBuilder(
-            animation: _logoController,
-            builder: (context, child) {
-              return Transform.translate(
-                offset: Offset(0, _logoJumpAnimation.value),
-                child: ScaleTransition(
-                  scale: _logoScaleAnimation,
-                  child: child,
+      backgroundColor: Colors.green[50],
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: Image.asset('assets/images/logo.png'),
                 ),
-              );
-            },
-            child: Image.asset(
-              'assets/images/logo.png',
-              height: 250, // Logo size
-              width: 250, // Logo size
-              fit: BoxFit.contain,
-              errorBuilder: (context, error, stackTrace) {
-                return const Icon(
-                  Icons.error,
-                  color: Colors.red,
-                  size: 100,
-                );
-              },
+                const Text(
+                  'StudyFlix',
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const CircularProgressIndicator(),
+                const SizedBox(height: 10),
+                const Text('version : 1.0.0')
+              ],
             ),
           ),
-        ),
+          Positioned(
+            bottom: 10,
+            left: MediaQuery.of(context).size.width / 4,
+            child: const Text(
+              'Developed by: Nitika Gurung',
+              style: TextStyle(fontSize: 15),
+            ),
+          ),
+        ],
       ),
     );
   }
