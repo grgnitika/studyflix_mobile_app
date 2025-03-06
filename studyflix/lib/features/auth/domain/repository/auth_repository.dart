036b5a09aad5b9185dc514e175d23f0d101c50de
grgnitika <gurungnitika59@gmail.com>
2025-03-06 22:50:20 +0545

    reposity  updates

--- studyflix/lib/features/auth/domain/repository/auth_repository.dart
+++ studyflix/lib/features/auth/domain/repository/auth_repository.dart
@@ -1,1 +1,15 @@
+import 'dart:io';
 
+import 'package:dartz/dartz.dart';
+import 'package:studyflix/core/error/failure.dart';
+import 'package:studyflix/features/auth/domain/entity/auth_entity.dart';
+
+abstract interface class IAuthRepository {
+  Future<Either<Failure, void>> registerUser(AuthEntity user);
+
+  Future<Either<Failure, String>> loginUser(String email, String password);
+
+  Future<Either<Failure, String>> uploadProfilePicture(File file);
+
+  Future<Either<Failure, AuthEntity>> getCurrentUser();
+}
